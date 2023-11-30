a75b7a52ab9f782296712ee3a9ebbf7d015106e6
Mirj <roth.mirjam@web.de>
2023-11-30 14:35:41 +0100

    Camera Edit + Collider for Items

---
+++ Assets/ClientListener.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnPlaceItem()
+    {
+
+    }
+    void OnPlayerTwoConnect()
+    {
+
+    }
+
+    void WaitForPlayerTwo()
+    {
+
+    }
+
+    void OnRemoveItem()
+    {
+
+    }
+
+    void OnUnlockPath()
+    {
+
+    }
+
+    void OnLoadPosition()
+    {
+
+    }
+
+    void OnUpdateItem() 
+    {
+    
+    }
+}
---
+++ Assets/InteractiveCube.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveCube : MonoBehaviour
+{
+    public GameObject iteminsert = new GameObject();
+    public bool collide = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iteminsert.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("An object entered.");
+        collide = true;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("An object is still inside of the trigger");
+        collide = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("An object left.");
+        collide = false;
+    }
+
+    void Update()
+    {
+        if (collide == true)
+        {
+            iteminsert.SetActive(true);
+        }
+        else
+        {
+            iteminsert.SetActive(false);
+        }
+    }
+
+}

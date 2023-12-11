3e748568f0c69c22fa18e04b6c2b171101f6f877
Mirj <roth.mirjam@web.de>
2023-12-11 15:36:09 +0100

    UI Sound Edit

---
+++ Assets/SoundOptions.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SoundOptions : MonoBehaviour
+{
+    [SerializeField] Slider soundslider;
+    [SerializeField] AudioMixer masterMixer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
+    }
+
+    public void SetVolume(float value)
+    {
+        if(value < 1)
+        {
+            value = .001f;
+        }
+
+        RefreshSlider(value);
+        PlayerPrefs.SetFloat("SavedMasterVolume", value);
+        masterMixer.SetFloat("MasterVolume", Mathf.Log10(value / 100) * 20f);
+    }
+
+    public void SetVolumeFromSlider()
+    {
+        SetVolume(soundslider.value);
+    }
+
+    public void RefreshSlider(float value)
+    {
+        soundslider.value = value;
+    }
+
+}
